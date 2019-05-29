4b93bda975b428e608cbfeb2e651e2a001e29c2d
Aleksey Baydyk <lehalevan@gmail.com>
2019-05-29 17:32:49 +0300

    mod_7

---
+++ js/module-07/js/index.js
@@ -0,0 +1,60 @@
+/*
+  1. Модифицируйте готовую функцию createPostCard() из задания 
+    номер 6 (https://codepen.io/goit-fe-adv/pen/MVPaeZ) так, 
+    чтобы она принимала объект post с данными для заполнения полей 
+    в карточке.
+      
+  2. Создайте функцию createCards(posts), которая принимает массив
+    объектов-карточек, вызывает функцию createPostCard(post) столько
+    раз, сколько объектов в массиве, сохраняя общий результат и возвращает 
+    массив DOM-элементов всех постов.
+    
+  3. Повесьте все посты в какой-то уже существующий DOM-узел.
+*/
+
+const posts = [
+  {
+    img: 'https://placeimg.com/400/150/arch',
+    title: 'Post title 1',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!',
+    link: 'link-1.com',
+  },
+  {
+    img: 'https://placeimg.com/400/150/nature',
+    title: 'Post title 2',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!',
+    link: 'link-2.com',
+  },
+  {
+    img: 'https://placeimg.com/400/150/arch',
+    title: 'Post title 3',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!',
+    link: 'link-3.com',
+  },
+];
+
+const createPostCard = function({ img, text, title, link }) {
+  const cardGallery = document.querySelector('.card-gallery');
+  const card = document.createElement('div');
+  const image = document.createElement('img');
+  image.setAttribute('src', img);
+  const header = document.createElement('h2');
+  header.textContent = title;
+  const desc = document.createElement('p');
+  desc.textContent = text;
+  const lnk = document.createElement('a');
+  lnk.setAttribute('href', link);
+  lnk.textContent = 'Read more';
+
+  card.append(image, header, desc, lnk);
+  cardGallery.append(card);
+};
+
+const createCards = function(posts) {
+  posts.forEach(post => createPostCard(post));
+};
+
+createCards(posts);
